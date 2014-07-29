82a7a33420712c7d4a3691f358b0196d49f6c158
Kyle Johnson <kjohnson@gnulnx.net>
2014-07-29 18:00:58 -0400

    Rework eventcmd.sh to... better.

--- eventcmd.sh
+++ eventcmd.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-# create variables
+host="http://127.0.0.1"
+port=3000
+baseurl="${host}:${port}"
+
+
+# Here be dragons! #
+# (Don't change anything below) #
 while read L; do
 	k="`echo "$L" | cut -d '=' -f 1`"
 	v="`echo "$L" | cut -d '=' -f 2`"
@@ -8,43 +14,39 @@ while read L; do
 done < <(grep -e '^\(title\|artist\|album\|stationName\|songStationName\|pRet\|pRetStr\|wRet\|wRetStr\|songDuration\|songPlayed\|rating\|coverArt\|stationCount\|station[0-9]*\)=' /dev/stdin) # don't overwrite $1...
 
 
+post () {
+	url=${baseurl}${1}
+	curl -s -XPOST $url >/dev/null 2>&1
+}
+
+clean () {
+	query=$1
+	clean=$(echo $query | sed 's/ /%20/g')
+	post $clean
+}
 
 
 case "$1" in
 	songstart)
-
-		echo "$title -- $artist" > $HOME/.config/pianobar/nowplaying
-
-	url="http://127.0.0.1:3000/?title=${title}&artist=${artist}&coverArt=${coverArt}&album=${album}"
-	echo $url >> /tmp/out
-
-	clean=$(echo $url | sed 's/ /%20/g')
-	echo $clean>> /tmp/out
-
-	curl -s -XPOST ${clean} >/dev/null 2>&1
-
-
-
-#		if [ "$rating" -eq 1 ]
-#		then
-#			kdialog --title pianobar --passivepopup "'$title' by '$artist' on '$album' - LOVED" 10
-#		else
-#			kdialog --title pianobar --passivepopup "'$title' by '$artist' on '$album'" 10
-#		fi
-#		# or whatever you like...
+		query="/start/?title=${title}&artist=${artist}&coverArt=${coverArt}&album=${album}"
+		clean "$query"
 		;;
 
 #	songfinish)
 #		;;
 
-#	songlove)
-#		;;
+	songlove)
+		query="/lovehate/?rating=${rating}"
+		clean $query
+		;;
 
 #	songshelf)
 #		;;
 
-#	songban)
-#		;;
+	songban)
+		query="/lovehate/?rating=${rating}"
+		clean $query
+		;;
 
 #	songbookmark)
 #		;;

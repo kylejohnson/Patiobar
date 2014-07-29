3a774af90f5ff39b6cc3d7c72704e294db0a6875
Kyle Johnson <kjohnson@gnulnx.net>
2014-07-29 18:01:28 -0400

    Initial support for sending commands to pianobar

--- index.js
+++ index.js
@@ -1,27 +1,76 @@
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var fs = require('fs');
+
+var fifo = '/tmp/pianobar-ctl';
 
 server.listen(3000);
 
 app.get('/', function(req, res){
-  res.sendfile('post.html');
+  res.sendfile('index.html');
 });
 
 io.on('connection', function(socket) {
 	console.log('a user connected');
 
+	socket.on('action', function (data) {
+		var action = data.action.substring(0, 1)
+		console.log(action);
+
+
+
+fs.open(fifo, 'w', 0644, function(error, fd) {
+  if (error) {
+    if (fd) {
+      fs.close(fd);
+    }
+    console.log('Error opening fifo: ' + error);
+    return;
+  }
+
+	buf = new Buffer(action);
+
+  fs.write(fd, buf, 0, action.length, null, function(error, written, buffer) {
+    if (fd) {
+      fs.close(fd);
+    }
+    if (error) {
+      console.log('Error writing to fifo: ' + error);
+    } else {
+      if (written == action.length) {
+        console.log('Input has been written successfully!');
+      } else {
+        console.log('Error: Only wrote ' + written + ' out of ' + action.length + ' bytes to fifo.');
+      }
+    }
+  });
+});
+
+
+
+
+
+	});
 	
 
 });
 
-app.post('/', function(request, response){
+app.post('/start', function(request, response){
 	artist = request.query.artist;
 	title = request.query.title;
 	album = request.query.album;
 	coverArt = request.query.coverArt;
 	
-	io.emit('pianobar', { artist: artist, title: title, coverArt: coverArt, album: album });
+	io.emit('start', { artist: artist, title: title, coverArt: coverArt, album: album });
 
 	response.send(request.query);
+});
+
+app.post('/lovehate', function(request, response) {
+	rating = request.query.rating;
+
+	io.emit('lovehate', { rating: rating });
+
+	console.log(request.query)
 });

392419090f6aa045da2a48f4e9c009e86c0fa2de
Kyle Johnson <kjohnson@gnulnx.net>
2014-08-17 15:45:38 -0400

    Use AngularJS

--- views/js/main.js
+++ views/js/main.js
@@ -1,14 +1,58 @@
-	var socket = io();
+var app = angular.module('patiobarApp', []);
 
+app.factory('socket', function ($rootScope) {
+  var socket = io.connect();
+  return {
+    on: function (eventName, callback) {
+      socket.on(eventName, function () {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          callback.apply(socket, args);
+        });
+      });
+    },
+    emit: function (eventName, data, callback) {
+      socket.emit(eventName, data, function () {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          if (callback) {
+            callback.apply(socket, args);
+          }
+        });
+      })
+    }
+  };
+});
+
+
+function StationController($scope, socket) {
+	socket.on('stations', function(msg) {
+		msg.stations.pop();
+		var s = [];
+
+		for (i in msg.stations) {
+			var array = msg.stations[i].split(":");
+			var id = array[0];
+			var name = array[1].replace("Radio", "");
+
+			s.push({name: name, id: id});
+		}
+
+		$scope.stations = s;
+	});
+
+	$scope.changeStation = function(stationId) {
+		socket.emit('changeStation', { stationId: stationId });
+	}
+}
+
+function SongController($scope, socket) {
 	socket.on('start', function(msg) {
-		$( "#coverArt" ).attr({
-			alt: msg.album,
-			src: msg.coverArt,
-			title: msg.album
-		});
-		$("#title").text(msg.title);
 		var aa = 'on ' + msg.album + ' by ' + msg.artist;
-		$("#albumartist").text(aa);
+		$scope.albumartist = aa;
+		$scope.src = msg.coverArt;
+		$scope.alt = msg.album;
+		$scope.title = msg.title;
 
 		if (msg.rating == 1) {
 			document.getElementById("love").className = "btn btn-success pull-left";
@@ -18,51 +62,19 @@
 		}
 	});
 
+	$scope.sendCommand = function(action) {
+		socket.emit('action', { action: action });
+	}
+
+	$("#pauseplay").click(function() {
+		$(this).children().toggleClass('glyphicon-pause glyphicon-play');
+	});
+
 	socket.on('lovehate', function(msg) {
 		if (msg.rating == 1) {
 			document.getElementById("love").className = "btn btn-success pull-left";
 		}
-
 	});
 
-	function appendStation(station) {
-		var s = document.getElementById('stations');
-		var b = document.createElement('button');
-		var stationId = station[0];
-		var stationName = station[1].replace("Radio", "");
 
-		var linkText = document.createTextNode(stationName);
-
-		b.appendChild(linkText);
-		b.setAttribute('type', 'button');
-		b.setAttribute('onClick', "changeStation('" + stationId + "')");
-		b.setAttribute('class', 'btn btn-default navbar-btn btn-sm');
-
-		s.appendChild(b);
-	}
-
-	function parseStation(stations) {
-		var a = stations.split(":");
-		appendStation(a);
-	}
-
-	socket.on('stations', function(msg) {
-		document.getElementById('stations').innerHTML = "";
-		msg.stations.pop();
-		for (i in msg.stations) {
-			parseStation(msg.stations[i]);
-		}
-	});
-
-	function sendCommand(action) {
-		socket.emit('action', { action: action });
-	}
-
-	function changeStation(stationId) {
-		socket.emit('changeStation', { stationId: stationId });
-	}
-
-	$("#pauseplay").click(function() {
-		$(this).children().toggleClass('glyphicon-pause');
-		$(this).children().toggleClass('glyphicon-play');
-	});
+}

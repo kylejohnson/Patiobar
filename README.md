ffa4a651be8da7825a709559ff025135ca0b24a6
Kyle Johnson <kjohnson@gnulnx.net>
2016-12-13 12:39:30 -0700

    Initial commit of install script and updated readme instructions.

--- README.md
+++ README.md
@@ -19,16 +19,13 @@ A great way to control Pandora / Pianobar on a Raspberry Pi
 
 Install
 -------
+Either
+`curl -s https://github.com/kylejohnson/Patiobar/blob/master/install.sh | bash`
+or 
 ```bash
 git clone https://github.com/kylejohnson/Patiobar.git
 cd Patiobar
-npm install
-mkfifo ctl
-mkdir -p ~/.config/pianobar
-cat << EOF >> ~/.config/pianobar/config
-event_command = ${PWD}/eventcmd.sh
-fifo = ${PWD}/ctl
-EOF
+bash install.sh
 ```
 
 Usage
@@ -37,7 +34,7 @@ Usage
 We assume that you've installed Patiobar to your users home directory!
 
 1. Follow the Install instructions above
-2. Start `pianobar` in a screen.  e.g. `screen -S pianobar -d -m pianobar`
+2. Start `pianobar`, perhaps in a screen.  e.g. `screen -S pianobar -d -m pianobar`
 3. Login, if you don't have pianobar set to do that automatically
 4. Select a station, if one isn't already playing or configured to auto-play
 5. Start Patiobar: `cd Patiobar && node index.js`
---
+++ install.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+tls_fingerprint=`openssl s_client -connect tuner.pandora.com:443 < /dev/null 2> /dev/null | openssl x509 -noout -fingerprint | tr -d ':' | cut -d'=' -f2`
+event_command=${PWD}/eventcmd.sh
+fifo=${PWD}/ctl
+configdir=~/.config/pianobar
+repo='https://github.com/kylejohnson/Patiobar.git'
+
+# Don't clone if already in the repo
+if [ ! -d .git ]; then
+	if git clone "${repo}"; then
+		cd Patiobar
+	else
+		echo "Could not clone ${repo}"
+		exit 1
+	fi
+fi
+
+# Don't run npm install if it has already ran
+if [ ! -d node_modules ]; then
+	echo -n 'Running `npm install`...   '
+	if npm install > /dev/null 2>&1; then
+		echo "success"
+	else
+		echo "failure"
+	fi
+fi
+
+# Don't create fifo if it already exists
+if [ ! -p "${fifo}" ]; then
+	echo -n "Creating ${fifo} control file...   "
+	if mkfifo "${fifo}" > /dev/null 2>&1; then
+		echo "success"
+	else
+		echo "failure"
+	fi
+fi
+
+echo -n "Creating default ~/.config/pianobar/config file...   "
+if mkdir -p "${configdir}"; then
+	cat << EOF >> "${configdir}/config"
+user = user@example.com
+password = password
+#autostart_station = 123456
+audio_quality = high
+event_command = ${event_command}
+fifo = ${fifo}
+tls_fingerprint = ${tls_fingerprint}
+EOF
+	echo "success"
+else
+	echo "failure"
+fi
